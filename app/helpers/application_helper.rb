9502a302a9b064447b2b5bd98673687d6cfe8a56
V.Bondaruk <slaff@pow.la>
2020-07-17 15:51:10 -0300

    Switch to fontawesome. Update homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def bs_icon(icon, options = {})
-    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
-    doc = Nokogiri::HTML::DocumentFragment.parse file
-    svg = doc.at_css 'svg'
-    svg['class']  += " " + options[:class] if options[:class].present?
-    svg['width']  += " " + options[:width].to_s if options[:width].present?
-    svg['height'] += " " + options[:height].to_s if options[:height].present?
-    viewbox = options[:viewbox].present? ? options[:viewbox].to_s : "0 0 16 16"
-    svg['viewbox'] += " " + viewbox
-    doc.to_html.html_safe
-  end  
 end
